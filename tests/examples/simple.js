14714c5439045e909fa3587a6035de773976b5fc
Jochen <info@testingbot.com>
2012-12-30 15:19:59 +0100

    basic selenium example test

---
+++ tests/examples/simple.js
@@ -0,0 +1,17 @@
+var webdriverjs = require('../../lib/tbwdjs');
+var client = webdriverjs.remote({
+    host: 'hub.testingbot.com',
+    desiredCapabilities: {
+        browserName: 'internet explorer',
+        version: 9,
+        platform: 'WINDOWS',
+        api_key: process.env.TESTINGBOT_KEY,
+        api_secret: process.env.TESTINGBOT_SECRET
+    }
+});
+
+client
+    .init()
+    .url('http://google.com/')
+    .titleEquals('Google')
+    .end();
